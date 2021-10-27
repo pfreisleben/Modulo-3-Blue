2728f0b0760f0d1a3267ce2971f36942f15cf8a9
pfreisleben <pedrofreislebeen@gmail.com>
2021-10-27 19:28:47 -0300

    ok

--- Projeto Semana 01/index.js
+++ Projeto Semana 01/index.js
@@ -1,14 +1,12 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; // Const para armanezar a porta do servidor
 const path = require('path');
-const { isNumberObject } = require('util/types');
 const filmes = require('./filmes');
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded());
+app.use(express.urlencoded({ extended: true }));
 // GET RAIZ
 app.get('/', async (req, res) => {
   res.send('Olá bluemer! Você acabou de enviar um GET para /');
@@ -32,6 +30,7 @@ app.get('/filmes/:id', async (req, res) => {
 
 app.post('/filmes', (req, res) => {
   const { numero, filme, lancamento, direcao } = req.body;
+  res.send(req.body);
 });
 
 app.listen(port, () =>
