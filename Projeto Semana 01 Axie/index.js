31a39a0a2dc78da9a8070d1d8c0af5c49879fe23
pfreisleben <pedrofreislebeen@gmail.com>
2021-10-28 20:04:38 -0300

    ok

--- Projeto Semana 01 Axie/index.js
+++ Projeto Semana 01 Axie/index.js
@@ -2,76 +2,77 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000; // Const para armanezar a porta do servidor
 const path = require('path');
-const filmes = require('./filmes');
+const axies = require('./axies');
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // GET RAIZ
 app.get('/', async (req, res) => {
   res.send('Olá bluemer! Você acabou de enviar um GET para /');
 });
 
-app.get('/filmes', async (req, res) => {
-  res.send(filmes.filter(Boolean));
+app.get('/axies', async (req, res) => {
+  res.send(axies.filter(Boolean));
 });
 
-app.get('/filmes/:id', async (req, res) => {
+app.get('/axies/:id', async (req, res) => {
   const id = --req.params.id;
-  const filme = filmes[id];
+  const axie = axies[id];
 
-  if (!filme) {
-    res.send('Filme não encontrado');
+  if (!axie) {
+    res.send('Axie não encontrado');
     return;
   }
 
-  res.send(filme);
+  res.send(axie);
 });
 
-app.post('/filmes', (req, res) => {
+app.post('/axies', (req, res) => {
   Object.keys(req.body).forEach((elemento) => {
     if (!req.body[elemento]) {
       return res.send('Por favor alimente todos os campos.');
     }
   });
 
-  const numero = filmes.length + 1;
-  const { filme, lancamento, direcao } = req.body;
+  const id = axies.length + 1;
+  const { classe, breed_count, owner, stats } = req.body;
 
-  filmes.push({
-    numero,
-    filme,
-    lancamento,
-    direcao,
+  axies.push({
+    id,
+    classe,
+    breed_count,
+    owner,
+    stats,
   });
-
-  res.send(filmes);
+  res.send(axies);
 });
 
-app.put('/filmes/:id', (req, res) => {
+app.put('/axies/:id', (req, res) => {
   Object.keys(req.body).forEach((elemento) => {
     if (!req.body[elemento]) {
       return res.send('Por favor alimente todos os campos.');
     }
   });
 
-  const id = req.params.id - 1;
-  const numero = req.params.id;
-  const { filme, lancamento, direcao } = req.body;
+  const numero = req.params.id - 1;
+  const id = req.params.id;
+  const { classe, breed_count, owner, stats } = req.body;
 
-  filmes[id] = {
-    numero,
-    filme,
-    lancamento,
-    direcao,
+  axies[numero] = {
+    id,
+    classe,
+    breed_count,
+    owner,
+    stats,
   };
-  res.send(filmes);
+  res.send(axies);
 });
 
-app.delete('/filmes/:id', (req, res) => {
+app.delete('/axies/:id', (req, res) => {
   const id = --req.params.id;
-  delete filmes[id];
-  res.send('Filme exclúido com sucesso');
+  delete axies[id];
+  res.send(axies);
 });
 
 app.listen(port, () =>
