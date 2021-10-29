5adf6dde4d7cd9368f12b24729c382435378e85b
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-10-29 08:31:58 -0300

    ok

---
+++ Desafio Técnico 28.10.2021/Projeto Semana 01 Axie/axies.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    id: 1,
+    classe: 'Aquatic',
+    breed_count: '4/7',
+    owner: 'Freisleben',
+    stats: {
+      health: 45,
+      speed: 57,
+      skill: 35,
+      morale: 27,
+    },
+  },
+  {
+    id: 2,
+    classe: 'Beast',
+    breed_count: '4/7',
+    owner: 'Freisleben',
+    stats: {
+      health: 31,
+      speed: 41,
+      skill: 31,
+      morale: 61,
+    },
+  },
+];
