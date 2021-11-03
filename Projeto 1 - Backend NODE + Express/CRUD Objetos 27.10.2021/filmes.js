ee94eb1cfdce9df44c2705c02450a4e10e4632ca
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-11-03 08:26:23 -0300

    ok

---
+++ Projeto 1 - Backend NODE + Express/CRUD Objetos 27.10.2021/filmes.js
@@ -0,0 +1,20 @@
+module.exports = [
+  {
+    numero: 01,
+    filme: 'Capitão América',
+    lancamento: '29/07/2011',
+    direcao: 'Joe Johnston',
+  },
+  {
+    numero: 02,
+    filme: 'Matrix',
+    lancamento: '21/05/1999',
+    direcao: 'Wachowski Sisters',
+  },
+  {
+    numero: 03,
+    filme: 'John Wick',
+    lancamento: '27/11/2014',
+    direcao: 'Chad Staheslki',
+  }
+];
