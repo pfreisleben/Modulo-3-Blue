b7c6bc4a959f6559755ca6444d81723932fdb752
pfreisleben <pedrofreislebeen@gmail.com>
2021-11-04 19:28:07 -0300

    ok

--- Projeto 2 - Integração Back + Mongo Atlas + Heroku/routers/cidades.routes.js
+++ Projeto 2 - Integração Back + Mongo Atlas + Heroku/routers/cidades.routes.js
@@ -6,11 +6,15 @@ router.get('/', (req, res) => {
   res.status(200).json({ message: 'Rota de cidades funcionando!' });
 });
 
-router.get('/listar', (req, res) => {
-  await Cidade.find({}).then((cidadess) => {
-    console.log(cidades);
-    res.status(200).json(cidades);
-  });
+router.get('/listar', async (req, res) => {
+  await Cidade.find({})
+    .then((cidades) => {
+      console.log(cidades);
+      res.status(200).json(cidades);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
