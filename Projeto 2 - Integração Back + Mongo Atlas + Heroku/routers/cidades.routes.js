4adbdf32a0749df0e54d4742e5731c544d3f5f41
pfreisleben <pedrofreislebeen@gmail.com>
2021-11-04 20:42:19 -0300

    ok

--- Projeto 2 - Integração Back + Mongo Atlas + Heroku/routers/cidades.routes.js
+++ Projeto 2 - Integração Back + Mongo Atlas + Heroku/routers/cidades.routes.js
@@ -6,7 +6,7 @@ router.get('/', (req, res) => {
   res.status(200).json({ message: 'Rota de cidades funcionando!' });
 });
 
-router.get('/listar', async (req, res) => {
+router.get('/listall', async (req, res) => {
   await Cidade.find({})
     .then((cidades) => {
       console.log(cidades);
@@ -15,6 +15,36 @@ router.get('/listar', async (req, res) => {
     .catch((err) => {
       console.log(err);
     });
+});
+
+router.get('/listname/:name', async (req, res) => {
+  const name = req.params.name;
+  const cidade = await Cidade.find({ nome: name });
+  if (cidade.length === 0) {
+    res.status(404).send({ message: 'Objeto não encontrado' });
+    return;
+  } else {
+    res.status(200).json(cidade);
+  }
+});
+
+router.post('/add', async (req, res) => {
+  const { nome, qtdBairros, populacao, dtAniversario } = req.body;
+
+  if (!nome || !qtdBairros || !populacao || !dtAniversario) {
+    res
+      .status(400)
+      .send({ messagem: 'Objeto inválido. Algum campo está com valor vazio.' });
+    return;
+  } else {
+    await Cidade.create(req.body)
+      .then(() => {
+        res.status(200).json({ message: 'Cadastrado com sucesso' });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 });
 
 module.exports = router;
