db488e58c2603c428176d3c68cb6848d554588d9
Pedro Freisleben <pedrofreislebeen@gmail.com>
2021-11-04 10:00:45 -0300

    ok

--- Projeto 2 - Integração Back + Mongo Atlas + Heroku/model/estados.js
+++ Projeto 2 - Integração Back + Mongo Atlas + Heroku/model/estados.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 
 const estadosModel = new mongoose.Schema({
   nome: { type: String, required: true },
-  regiao: { type: Number, required: true },
+  regiao: { type: String, required: true },
   populacao: { type: Number, required: true },
-  vlrSalarioMin: { type: Date, required: true },
+  vlrSalarioMin: { type: Number, required: true },
 });
 
 const Estado = mongoose.model('Estados', estadosModel);
 
-module.exports = Cidade;
+module.exports = Estado;
